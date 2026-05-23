ec855c0d0743dc4a43521192c189c192c4a09ed2
Sam C <saacifuentesmu@gmail.com>
2026-05-23 07:12:39 -0500

    chore(config): eslint, gitignore, env templates, RN config typing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:import/recommended', 'plugin:import/typescript'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/order': ['warn', {'newlines-between': 'always'}],
+  },
 };
